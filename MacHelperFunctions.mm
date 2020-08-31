984080de61a13d95aa9e9ea84f8c3fea681baa7e
Nick Zitzmann <nickzman@gmail.com>
2020-08-30 23:39:45 -0500

    Fix sound engine crash & adjust deployment target in the test app

--- MacHelperFunctions.mm
+++ MacHelperFunctions.mm
@@ -8,12 +8,13 @@
 
 #import <Foundation/Foundation.h>
 #import "MacHelperFunctions.h"
+#import "RSSSkyrocketSaverView.h"
 
 const char *PathForResourceOfType(const char *resourceName, const char *type)
 {
 	NSString *resourceNameString = [[NSFileManager defaultManager] stringWithFileSystemRepresentation:resourceName length:strlen(resourceName)];
 	NSString *typeString = [[NSFileManager defaultManager] stringWithFileSystemRepresentation:type length:strlen(type)];
-	NSString *path = [[NSBundle bundleWithIdentifier:@"com.reallyslick.Skyrocket"] pathForResource:resourceNameString ofType:typeString];
+	NSString *path = [[NSBundle bundleForClass:[RSSSkyrocketSaverView class]] pathForResource:resourceNameString ofType:typeString];
 	
 	if (path)
 		return [path fileSystemRepresentation];
